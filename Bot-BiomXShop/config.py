e2b04b2b07dd7fdde18c8e9be5bcd112c119102f
BiomX95 <imran.best95@mail.ru>
2025-12-02 01:36:04 +0300

    Update image paths in config.py

--- Bot-BiomXShop/config.py
+++ Bot-BiomXShop/config.py
@@ -1,8 +1,8 @@
 TOKEN = "7650590553:AAEDeGA3b_1CfxTD6Aws5EoOy_vIi6QlVVw"
 # Путь к картинкам аренды
-IMG_PATH = "C:/Users/РС/OneDrive/Desktop/Bot-BiomXShop/images/"
+IMG_PATH = "images/"
 # Путь к картинкам логотипов / общего назначения
-RENT_IMG = "C:/Users/РС/OneDrive/Desktop/Bot-BiomXShop/images/rent/"
+RENT_IMG = "images/rent/"
 # Путь к картинкам алмазов
 DONATE_IMG = "images/donate/"
 
@@ -11,4 +11,5 @@ CHAT_IDS = [
     -1002379198855   # чат Дарьи борисенко №2 
 ]
 AUTOPOST_INTERVAL_SECONDS = 270
+
 
