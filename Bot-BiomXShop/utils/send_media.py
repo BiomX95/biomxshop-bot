8310d9cf79da7996a35156807ee1e37b622deafa
BiomX95 <imran.best95@mail.ru>
2025-12-02 01:21:30 +0300

    Update send_media.py

--- Bot-BiomXShop/utils/send_media.py
+++ Bot-BiomXShop/utils/send_media.py
@@ -1,8 +1,15 @@
+import os
 from telebot import types
+from config import RENT_IMG
 
 def send_two_photos(bot, message, photo1, photo2, caption):
+    # photo1 и photo2 — просто имена файлов
+    path1 = os.path.join(RENT_IMG, photo1)
+    path2 = os.path.join(RENT_IMG, photo2)
+
     media = [
-        types.InputMediaPhoto(open(photo1, "rb")),
-        types.InputMediaPhoto(open(photo2, "rb"), caption=caption, parse_mode="HTML")
+        types.InputMediaPhoto(open(path1, "rb")),
+        types.InputMediaPhoto(open(path2, "rb"), caption=caption, parse_mode="HTML")
     ]
+
     bot.send_media_group(message.chat.id, media)
