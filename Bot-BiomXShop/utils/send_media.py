593960144feaa480cd7239d42da59fb0e128a2fb
BiomX95 <imran.best95@mail.ru>
2025-12-04 00:15:57 +0300

    Remove unnecessary whitespace in send_media.py

--- Bot-BiomXShop/utils/send_media.py
+++ Bot-BiomXShop/utils/send_media.py
@@ -5,7 +5,6 @@ def send_two_photos(bot, message, path1, path2, caption):
         types.InputMediaPhoto(open(path1, "rb")),
         types.InputMediaPhoto(open(path2, "rb"))
     ]
-    
+
     bot.send_media_group(message.chat.id, media)
     bot.send_message(message.chat.id, caption)
-
