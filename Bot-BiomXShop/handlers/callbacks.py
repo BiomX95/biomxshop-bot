cea2f537f972863bd915af8371864e4455af452b
BiomX95 <imran.best95@mail.ru>
2025-12-03 21:45:51 +0300

    Update callbacks.py

--- Bot-BiomXShop/handlers/callbacks.py
+++ Bot-BiomXShop/handlers/callbacks.py
@@ -8,67 +8,71 @@ def register_handlers(bot, is_private=False):
     @bot.callback_query_handler(func=lambda call: call.message and call.message.chat.type == "private")
     def callback(call):
 
-        # Вспомогательная функция для корректного формирования пути
+        # Вспомогательная функция для корректного формирования пути и проверки файла
         def rent_path(filename):
-            return os.path.join(RENT_IMG, filename)
+            path = os.path.join(RENT_IMG, filename)
+            if not os.path.exists(path):
+                print(f"Файл не найден: {path}")
+                return None
+            return path
 
         # RENTS
         if call.data == "rent1":
-            send_two_photos(
-                bot, call.message,
-                rent_path("Abakaev.jpg"),
-                rent_path("Abakaev.jpg"),
-                descriptions[1]
-            )
+            file1 = rent_path("Abakaev.jpg")
+            file2 = rent_path("Abakaev.jpg")
+            if file1 and file2:
+                send_two_photos(bot, call.message, file1, file2, descriptions[1])
+            else:
+                bot.send_message(call.message.chat.id, "Извините, фото временно недоступны.")
 
         elif call.data == "rent2":
-            send_two_photos(
-                bot, call.message,
-                rent_path("KARINA.jpg"),
-                rent_path("KARINAevo.jpg"),
-                descriptions[2]
-            )
+            file1 = rent_path("KARINA.jpg")
+            file2 = rent_path("KARINAevo.jpg")
+            if file1 and file2:
+                send_two_photos(bot, call.message, file1, file2, descriptions[2])
+            else:
+                bot.send_message(call.message.chat.id, "Извините, фото временно недоступны.")
 
         elif call.data == "rent3":
-            send_two_photos(
-                bot, call.message,
-                rent_path("BiomXShop.jpg"),
-                rent_path("BiomXShop2.jpg"),
-                descriptions[3]
-            )
+            file1 = rent_path("BiomXShop.jpg")
+            file2 = rent_path("BiomXShop2.jpg")
+            if file1 and file2:
+                send_two_photos(bot, call.message, file1, file2, descriptions[3])
+            else:
+                bot.send_message(call.message.chat.id, "Извините, фото временно недоступны.")
 
         elif call.data == "rent4":
-            send_two_photos(
-                bot, call.message,
-                rent_path("malish.jpg"),
-                rent_path("malish2.jpg"),
-                descriptions[4]
-            )
-            
+            file1 = rent_path("malish.jpg")
+            file2 = rent_path("malish2.jpg")
+            if file1 and file2:
+                send_two_photos(bot, call.message, file1, file2, descriptions[4])
+            else:
+                bot.send_message(call.message.chat.id, "Извините, фото временно недоступны.")
+
         elif call.data == "rent5":
-            send_two_photos(
-                bot, call.message,
-                rent_path("DAGPropysk.jpg"),
-                rent_path("DAGPropysk2.jpg"),
-                descriptions[5]
-            )
-            
+            file1 = rent_path("DAGPropysk.jpg")
+            file2 = rent_path("DAGPropysk2.jpg")
+            if file1 and file2:
+                send_two_photos(bot, call.message, file1, file2, descriptions[5])
+            else:
+                bot.send_message(call.message.chat.id, "Извините, фото временно недоступны.")
+
         elif call.data == "rent6":
-            send_two_photos(
-                bot, call.message,
-                rent_path("yasmi.jpg"),
-                rent_path("yasmi.jpg"),
-                descriptions[6]
-            )
-            
+            file1 = rent_path("yasmi.jpg")
+            file2 = rent_path("yasmi.jpg")
+            if file1 and file2:
+                send_two_photos(bot, call.message, file1, file2, descriptions[6])
+            else:
+                bot.send_message(call.message.chat.id, "Извините, фото временно недоступны.")
+
         elif call.data == "rent7":
-            send_two_photos(
-                bot, call.message,
-                rent_path("tyrpalpropusk.jpg"),
-                rent_path("tyrpalpropusk.jpg"),
-                descriptions[7]
-            )
-            
+            file1 = rent_path("tyrpalpropusk.jpg")
+            file2 = rent_path("tyrpalpropusk.jpg")
+            if file1 and file2:
+                send_two_photos(bot, call.message, file1, file2, descriptions[7])
+            else:
+                bot.send_message(call.message.chat.id, "Извините, фото временно недоступны.")
+
         elif call.data == "rent8":
             bot.send_message(call.message.chat.id, descriptions[8])
 
@@ -88,7 +92,6 @@ def register_handlers(bot, is_private=False):
                    "xtls-rprx-vision&encryption=none#avovpn.com")
             bot.answer_callback_query(call.id, "Ключ отправлен")
             bot.send_message(call.message.chat.id, key)
-
 
 
 
