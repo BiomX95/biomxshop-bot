37457d4800aa2caf6d40a0e280381e64e4ee6fd0
BiomX95 <imran.best95@mail.ru>
2025-12-04 00:14:50 +0300

    Refactor send_two_photos to use direct file paths
    Updated send_two_photos function to accept file paths directly and send a caption as a separate message.

--- Bot-BiomXShop/utils/send_media.py
+++ Bot-BiomXShop/utils/send_media.py
@@ -1,15 +1,11 @@
-import os
 from telebot import types
-from config import RENT_IMG
 
-def send_two_photos(bot, message, photo1, photo2, caption):
-    # photo1 и photo2 — просто имена файлов
-    path1 = os.path.join(RENT_IMG, photo1)
-    path2 = os.path.join(RENT_IMG, photo2)
-
+def send_two_photos(bot, message, path1, path2, caption):
     media = [
         types.InputMediaPhoto(open(path1, "rb")),
-        types.InputMediaPhoto(open(path2, "rb"), caption=caption, parse_mode="HTML")
+        types.InputMediaPhoto(open(path2, "rb"))
     ]
-
+    
     bot.send_media_group(message.chat.id, media)
+    bot.send_message(message.chat.id, caption)
+
