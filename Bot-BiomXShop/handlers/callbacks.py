54298f0082b47cac8602be79b5458a3d5a4eb3d8
BiomX95 <imran.best95@mail.ru>
2025-12-03 19:19:37 +0300

    Refactor callback handlers to use RENT_IMG constant

--- Bot-BiomXShop/handlers/callbacks.py
+++ Bot-BiomXShop/handlers/callbacks.py
@@ -1,41 +1,72 @@
 from utils.send_media import send_two_photos
+from config import RENT_IMG
 from texts.rent_texts import descriptions
 
-def register_handlers(bot, is_private):
+def register_handlers(bot, is_private=False):
 
-    @bot.callback_query_handler(func=lambda call: True)
+    @bot.callback_query_handler(func=lambda call: call.message and call.message.chat.type == "private")
     def callback(call):
 
-        # ГЛАВНАЯ защита
-        if call.message.chat.type != "private":
-            bot.answer_callback_query(call.id)
-            return
-
+        # RENTS
         if call.data == "rent1":
-            send_two_photos(bot, call.message, "Abakaev.jpg", "Abakaev.jpg", descriptions[1])
+            send_two_photos(
+                bot, call.message,
+                RENT_IMG + "Abakaev.jpg",
+                RENT_IMG + "Abakaev.jpg",
+                descriptions[1]
+            )
 
         elif call.data == "rent2":
-            send_two_photos(bot, call.message, "KARINA.jpg", "KARINAevo.jpg", descriptions[2])
+            send_two_photos(
+                bot, call.message,
+                RENT_IMG + "KARINA.jpg",
+                RENT_IMG + "KARINAevo.jpg",
+                descriptions[2]
+            )
 
         elif call.data == "rent3":
-            send_two_photos(bot, call.message, "BiomXShop.jpg", "BiomXShop2.jpg", descriptions[3])
+            send_two_photos(
+                bot, call.message,
+                RENT_IMG + "BiomXShop.jpg",
+                RENT_IMG + "BiomXShop2.jpg",
+                descriptions[3]
+            )
 
         elif call.data == "rent4":
-            send_two_photos(bot, call.message, "malish.jpg", "malish2.jpg", descriptions[4])
+            send_two_photos(
+                bot, call.message,
+                RENT_IMG + "malish.jpg",
+                RENT_IMG + "malish2.jpg",
+                descriptions[4]
+            )
 
         elif call.data == "rent5":
-            send_two_photos(bot, call.message, "DAGPropysk.jpg", "DAGPropysk2.jpg", descriptions[5])
+            send_two_photos(
+                bot, call.message,
+                RENT_IMG + "DAGPropysk.jpg",
+                RENT_IMG + "DAGPropysk2.jpg",
+                descriptions[5]
+            )
 
         elif call.data == "rent6":
             bot.send_message(call.message.chat.id, descriptions[6])
 
+        # VPN keys
         elif call.data == "vpn_ios":
+            key = ("vless://cb55c987-9e1e-4a2b-8cea-e429c6bb530b@94.198.100.173:443"
+                   "?security=reality&sni=aws.com&alpn=h2&fp=chrome&pbk="
+                   "ckRcueERkPqqjZABwxqni_J_Nbb70Q6k5fEEUAjoImw&type=tcp&flow="
+                   "xtls-rprx-vision&encryption=none#avovpn.com")
             bot.answer_callback_query(call.id, "Ключ отправлен")
-            bot.send_message(call.message.chat.id, KEY_STRING)
+            bot.send_message(call.message.chat.id, key)
 
         elif call.data == "vpn_android":
+            key = ("vless://cb55c987-9e1e-4a2b-8cea-e429c6bb530b@94.198.100.173:443"
+                   "?security=reality&sni=aws.com&alpn=h2&fp=chrome&pbk="
+                   "ckRcueERkPqqjZABwxqni_J_Nbb70Q6k5fEEUAjoImw&type=tcp&flow="
+                   "xtls-rprx-vision&encryption=none#avovpn.com")
             bot.answer_callback_query(call.id, "Ключ отправлен")
-            bot.send_message(call.message.chat.id, KEY_STRING)
+            bot.send_message(call.message.chat.id, key)
 
 
 
