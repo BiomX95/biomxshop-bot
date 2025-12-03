66027cc84b8d8721c8239a3eaf6762d67366c304
BiomX95 <imran.best95@mail.ru>
2025-12-04 00:05:32 +0300

    Update callbacks.py

--- Bot-BiomXShop/handlers/callbacks.py
+++ Bot-BiomXShop/handlers/callbacks.py
@@ -2,10 +2,12 @@ from utils.send_media import send_two_photos
 from config import RENT_IMG
 from texts.rent_texts import descriptions
 
-def register_handlers(bot):
-    @bot.callback_query_handler(func=lambda c: True)
+def register_handlers(bot, is_private=False):
+
+    @bot.callback_query_handler(func=lambda call: call.message and call.message.chat.type == "private")
     def callback(call):
 
+        # RENTS
         if call.data == "rent1":
             send_two_photos(
                 bot, call.message,
@@ -49,20 +51,22 @@ def register_handlers(bot):
         elif call.data == "rent6":
             bot.send_message(call.message.chat.id, descriptions[6])
 
-        # -------- VPN: отправка ключей без ошибок -------- #
-
+        # VPN keys
         elif call.data == "vpn_ios":
-            key = (
-                "vless://cb55c987-9e1e-4a2b-8cea-e429c6bb530b@94.198.100.173:443?security=reality&sni=aws.com&alpn=h2&fp=chrome&pbk=ckRcueERkPqqjZABwxqni_J_Nbb70Q6k5fEEUAjoImw&type=tcp&flow=xtls-rprx-vision&encryption=none#avovpn.com-4636825-3414822")
-
+            key = ("vless://cb55c987-9e1e-4a2b-8cea-e429c6bb530b@94.198.100.173:443"
+                   "?security=reality&sni=aws.com&alpn=h2&fp=chrome&pbk="
+                   "ckRcueERkPqqjZABwxqni_J_Nbb70Q6k5fEEUAjoImw&type=tcp&flow="
+                   "xtls-rprx-vision&encryption=none#avovpn.com")
             bot.answer_callback_query(call.id, "Ключ отправлен")
             bot.send_message(call.message.chat.id, key)
 
         elif call.data == "vpn_android":
-            key = (
-                 "vless://cb55c987-9e1e-4a2b-8cea-e429c6bb530b@94.198.100.173:443?security=reality&sni=aws.com&alpn=h2&fp=chrome&pbk=ckRcueERkPqqjZABwxqni_J_Nbb70Q6k5fEEUAjoImw&type=tcp&flow=xtls-rprx-vision&encryption=none#avovpn.com-4636825-3414822")
-
+            key = ("vless://cb55c987-9e1e-4a2b-8cea-e429c6bb530b@94.198.100.173:443"
+                   "?security=reality&sni=aws.com&alpn=h2&fp=chrome&pbk="
+                   "ckRcueERkPqqjZABwxqni_J_Nbb70Q6k5fEEUAjoImw&type=tcp&flow="
+                   "xtls-rprx-vision&encryption=none#avovpn.com")
             bot.answer_callback_query(call.id, "Ключ отправлен")
             bot.send_message(call.message.chat.id, key)
+
 
 
